9c7bebe13f3c665d22c00746260843b87e6d7779
Baron Skidmore <baronhockeyplayer@gmail.com>
2022-03-09 11:42:35 -0500

    added fetch request and styling

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 var spoonacularAPIKey = 'cc4c59b381934298b2f27908bf302eb4';
-var cocktailAPI = 'www.thecocktaildb.com/api/json/v1/1/random.php'
 
 function fetchAPI(cocktailAPI) {
-    fetch(cocktailAPI)
-      .then(function (response) {
-        let data = response.json();
-        return data;
-      })
-      .then(function (data) {
-        console.log(data)
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-    }
+  var cocktailAPI = 'www.thecocktaildb.com/api/json/v1/1/random.php'
+  fetch(cocktailAPI)
+  .then(function (response) {
+    let data = response.json();
+    return data;
+  })
+  .then(function (data) {
+    console.log(data)
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+}
+
+function fetchSpoonByIngredients(spoonacularURL) {
+  var spoonacularURL = 'https://api.spoonacular.com/recipes/findByIngredients'
+  fetch(spoonacularURL)
+    .then(function (response) {
+      var data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+}
