b4bdd71dee96d3ff4175070a0bba11a0326e492d
Baron Skidmore <baronhockeyplayer@gmail.com>
2022-03-10 10:42:49 -0500

    added new html files and script for new pages

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
 var spoonacularAPIKey = 'cc4c59b381934298b2f27908bf302eb4';
 
-function fetchAPI(cocktailAPI) {
-  var cocktailAPI = 'www.thecocktaildb.com/api/json/v1/1/random.php'
+function fetchCocktails(name) {
+  var cocktailAPI = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+ 
   fetch(cocktailAPI)
   .then(function (response) {
-    let data = response.json();
+    var data = response.json();
     return data;
+
   })
   .then(function (data) {
-    console.log(data)
+    for (let i = 0; i < 10; i++) {
+    
+      console.log(data.drinks[i])
+    }
   })
   .catch(function (err) {
     console.log(err);
@@ -50,3 +55,6 @@ function getRecipeInfo(recipeInfo) {
   })
 }
 getRecipeInfo();
+
+
+
