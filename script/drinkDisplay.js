deb542361d6be8b80b1a112ac3b4871d6ab67e15
Baron Skidmore <baronhockeyplayer@gmail.com>
2022-03-11 12:52:12 -0500

    finalized logic for drink display page

--- script/drinkDisplay.js
+++ script/drinkDisplay.js
@@ -10,19 +10,48 @@ function fetchAndDisplayDrinkForId(id) {
         .then(function (data) {
             var drink = data.drinks[0]
             console.log(drink)
-            
+
             var alcoholic = drink.strAlcoholic
             var glass = drink.strGlass
             var instructions = drink.strInstructions
-            var thumb = strDrinkThumb
+            var thumb = drink.strDrinkThumb
+            var drinkName = drink.strDrink
+
+            var instructionContainer = document.querySelector('#instructioncontainer')
+            var drinkNameEl = document.createElement('h2')
+            drinkNameEl.textContent = drinkName
+            var alcoholicEl = document.createElement('p')
+            alcoholicEl.textContent = alcoholic;
+
+            var glassEl = document.createElement('p')
+            glassEl.textContent = glass
+            var instructionEl = document.createElement('p')
+            instructionEl.textContent = instructions
+            var thumbEl = document.createElement('img')
+            thumbEl.src = thumb;
+
+            instructionContainer.appendChild(drinkNameEl)
+            instructionContainer.appendChild(thumbEl)
+            instructionContainer.appendChild(alcoholicEl)
+            instructionContainer.appendChild(glassEl)
+            instructionContainer.appendChild(instructionEl)
 
             for (let i = 1; i <= 15; i++) {
+
                 if (drink[`strIngredient${i}`] === null) {
                     break
-                    
+
                 }
                 var ingredient = drink[`strIngredient${i}`]
                 var measurement = drink[`strMeasure${i}`]
+
+                var ingredientEl = document.createElement('p')
+                ingredientEl.textContent = ingredient
+                var measurementEl = document.createElement('p')
+                measurementEl.textContent = measurement
+
+                instructionContainer.appendChild(ingredientEl)
+                instructionContainer.appendChild(measurementEl)
             }
         })
         .catch(function (err) {
@@ -33,5 +62,8 @@ function fetchAndDisplayDrinkForId(id) {
 var queryString = window.location.search
 var urlParams = new URLSearchParams(queryString)
 var id = urlParams.get('id')
+
+
+
 
 fetchAndDisplayDrinkForId(id)
